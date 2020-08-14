5c5f60d8b38eb46b8b158da56a36ea63d8de02de
BPitts8019 <bradleyj.pitts@gmail.com>
2020-08-14 16:32:38 -0700

    Ex1: Calculate cache from list of weights and get a result; first pass

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,18 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+
+    for (index, weight) in enumerate(weights):
+        cache[weight] = index
+
+    for weight in weights:
+        other_weight = limit - weight
+        if other_weight in cache:
+            return (weight, other_weight)
 
     return None
+
+
+if __name__ == "__main__":
+    print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 32))
