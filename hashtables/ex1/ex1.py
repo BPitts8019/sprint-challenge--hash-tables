f834fd00e64018f22697e0daf4505e12d2588a30
BPitts8019 <bradleyj.pitts@gmail.com>
2020-08-14 16:56:12 -0700

    Ex1: Reverse greater-that/less-than rule for output

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -10,13 +10,15 @@ def get_indices_of_item_weights(weights, length, limit):
     for weight in weights:
         other_weight = limit - weight
         if other_weight in cache:
-            if cache[weight] < cache[other_weight]:
+            if cache[weight] > cache[other_weight]:
                 return (cache[weight], cache[other_weight])
-            elif cache[weight] > cache[other_weight]:
+            elif cache[weight] < cache[other_weight]:
                 return (cache[other_weight], cache[weight])
 
     return None
 
 
 if __name__ == "__main__":
-    print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 32))
+    print(get_indices_of_item_weights([9], 1, 9))
+    print(get_indices_of_item_weights([4, 4], 2, 8))
+    print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
