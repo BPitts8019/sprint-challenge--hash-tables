407f6f49abf44af0233653ebce416653d0cbbe60
BPitts8019 <bradleyj.pitts@gmail.com>
2020-08-14 18:51:42 -0700

    Ex1: Correct duplicate weight test failure

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,18 +7,10 @@ def get_indices_of_item_weights(weights, length, limit):
     for (index, weight) in enumerate(weights):
         cache[weight] = index
 
-    for weight in weights:
+    for cur_index, weight in enumerate(weights):
         other_weight = limit - weight
         if other_weight in cache:
-            if cache[weight] > cache[other_weight]:
-                return (cache[weight], cache[other_weight])
-            elif cache[weight] < cache[other_weight]:
-                return (cache[other_weight], cache[weight])
-
-    return None
-
-
-if __name__ == "__main__":
-    print(get_indices_of_item_weights([9], 1, 9))
-    print(get_indices_of_item_weights([4, 4], 2, 8))
-    print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
+            if cur_index > cache[other_weight]:
+                return (cur_index, cache[other_weight])
+            else:
+                return (cache[other_weight], cur_index)
